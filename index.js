14e179b99fe026639730e828d977f72c79e98dc4
pmg <gachigipm@gmail.com>
2024-07-22 10:33:25 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+var cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph)
+{cats.push(Ralph)};
+function destructivelyPrependCat(Ralph){
+    cats.unshift(Ralph);
+}
+function destructivelyRemoveLastCat(){
+    cats.splice(2);
+}
+function destructivelyRemoveFirstCat(){
+    cats.splice(0,1);
+}
+ function appendCat(){
+const apnd=[...cats,"Broom"];
+return apnd;
+ }
+ {[...cats,"Broom"];
+ }
+ function prependCat(Arnold){
+    const prep=["Arnold",...cats];
+    return prep;
+ }
+ function removeLastCat(){
+   const newcats=cats.slice(0,cats.length -1)
+   return newcats;
+ }
+ function removeFirstCat() {
+    const [, ...remainingCats] = cats;
+    return remainingCats;
+  }
